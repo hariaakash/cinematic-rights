d6b7d3ec4ba0f747e443783a58627c446f8806a2
Hari Narayanan <smgdark@gmail.com>
2024-03-13 18:26:32 +0000

    feat: home

--- frontend/src/lib/components/nav.svelte
+++ frontend/src/lib/components/nav.svelte
@@ -4,7 +4,7 @@
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img class="h-8" alt="Flowbite Logo">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Product<span class="text-sky-500">AI</span></span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinematic <span class="text-sky-500">Rights</span></span>
     </a>
     <!-- <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
