6edf1b4577bc4e63db9403edac09aa9a18816ea5
Hari Narayanan <smgdark@gmail.com>
2024-03-13 18:08:09 +0000

    feat: init pages

---
+++ frontend/src/lib/components/sidebar.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col">
+    <div class="flex-grow bg-gray-800 p-4 rounded">
+      <h2 class="text-lg font-semibold mb-2">Guideline Settings</h2>
+      <div class="mb-4">
+        <h3 class="font-semibold">Include:</h3>
+        <textarea
+          class="w-full h-32 bg-gray-700 text-white p-2 rounded outline-none"
+          placeholder="Enter text to include..."
+        ></textarea>
+      </div>
+    </div>
+    <div class="flex-grow bg-gray-800 p-4 rounded">
+      <h2 class="text-lg font-semibold mb-2">Guideline Settings</h2>
+      <div>
+        <h3 class="font-semibold">Subtract:</h3>
+        <textarea
+          class="w-full h-32 bg-gray-700 text-white p-2 rounded outline-none"
+          placeholder="Enter text to subtract..."
+        ></textarea>
+      </div>
+    </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex items-center justify-center bg-black">
     <div class="text-white text-center">
         <h1 class="text-3xl uppercase mb-8 bg-gradient-to-r from-white to-sky-500 inline-block text-transparent bg-clip-text">Welcome</h1>
-        <h3 class="mb-6">Cinematic Rights <br> "Empowering Creators, Connecting Worlds: Where Content Meets Creativity"</h3>
+        <h3 class="mb-6">Cinematic Rights <br> Empowering Creators, Connecting Worlds: Where Content Meets Creativity</h3>
         <a href="/home">
             <button class="bg-transparent rounded-full bg-gradient-to-r from-white to-sky-500 inline-block text-transparent bg-clip-text font-semibold py-2 px-4 border hover:border-sky-500">
                 Continue
